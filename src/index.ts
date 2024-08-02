8a5c1cc47cdb687e5bde43afbc7f8603b3fdb49a
Andres Hernandez <pipehernandez1998@gmail.com>
2024-08-01 21:38:33 -0500

    Kick off

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mainRouter from './routes/Router';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api', mainRouter);
+
+app.listen(PORT, () => {
+  console.clear();
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
